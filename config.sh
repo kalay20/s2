c45b0d514f58d5bd3a54c39d0af636492ecc0bd3
sin19682004 <sin19682004@gmail.com>
2015-05-22 05:33:26 -0700

    config

--- config.sh
+++ config.sh
@@ -8,8 +8,9 @@ if [ $user == "LaiYang" ]
 then
 	echo "Welcome! LaiYang."
 	sed -i -e "s/sysC-2.3.0/systemc-2.3.0/g" ./Makefile
-elif [ "$user" == "" ]
+elif [ "$user" == "JHLin" ]
 then
+	echo "Welcome! J.H. Lin."
 	sed -i -e "s/systemc-2.3.0/sysC-2.3.0/g" Makefile
 
 fi
