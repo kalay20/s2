2b29bd9daac775a31f51c7bed1da03ae2c6c862b
sin19682004 <sin19682004@gmail.com>
2015-05-24 06:02:50 -0700

    +gen_dir

---
+++ gen_dir.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+prefix=(wl_die block_size_config 3_factor el_die bs_die)
+d1=( config exec out )
+
+
+mkdir trace
+
+for (( i=0; i<3; i++ ))
+do
+	mkdir "${d1["$i"]}"
+	for (( j=0; j<5; j++ ))
+	do
+		mkdir "${d1["$i"]}"/"${prefix["$j"]}"
+	done
+done
+
+
+
+
+
+
+
+
+
+
