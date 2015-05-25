0a8eb0eecb19530d6619eb658309a65c1ec04c26
sin19682004 <sin19682004@gmail.com>
2015-05-24 22:16:35 -0700

    small edit

---
+++ trace/edit_all_trace.sh
@@ -0,0 +1,20 @@
+
+
+
+wd_start=${wd_start:-0}
+wd_end=${wd_end:-6}
+
+workload_burst=( mse_b msn_b msl_b usr1_b src10_b synf_b )
+workload_dst=( mse msn msl usr1 src10 synf )
+
+
+for (( wn="$wd_start"; wn<"$wd_end"; wn++ ))
+do
+	./a.out "${workload_dst["$wn"]}" "${workload_burst["$wn"]}"
+done
+
+
+
+
+
+
