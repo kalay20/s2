bf2250b6c381dbd69874fd4505ea0efb188767ca
sin19682004 <sin19682004@gmail.com>
2015-07-19 22:46:36 -0700

    m

--- config.sh
+++ config.sh
@@ -29,19 +29,41 @@ config_dir="config/${prefix["$change"-1]}"
 exec_dir="exec/${prefix["$change"-1]}"
 out_dir="out/${prefix["$change"-1]}"
 
-
+: << END
 is_config=${is_config:-1}
 source ./param.sh
 
-function Re {
-	index=${2:-()}
-	#index=$2
+IssueCommand()
+{
+	local pt=$1
+	local index=("${!2}")
+	local name
+	local t
+	local p
+	local i
+	local pA
+	local s
+	
+	for (( i=0; i<$pt; i++ ))
+	do
+		t=${index[$i]}; s="p=\${pA$i[\$t]}"; eval $s
+		name=$name"_"$p
+	done
+	
+	echo "name" $name
+}
+
+index=()
+Re()
+{
+	#local index=${2:-()}
 	if (( $1==$pt ))
 	then
-		t1=${index[0]}; t2=${pA0[$t1]}
-		printf "%d" "$t2"
-		t1=${index[1]}; t2=${pA1[$t1]}
-		printf "_%d\n" "$t2"
+		#t1=${index[0]}; t2=${pA0[$t1]}
+		#printf "%d" "$t2"
+		#t1=${index[1]}; t2=${pA1[$t1]}
+		#printf "_%d\n" "$t2"
+		IssueCommand $pt index[@]
 		return
 	fi
 	#m=0
@@ -51,15 +73,17 @@ function Re {
 	for (( p_i=${p_start[$1]}; p_i<${p_end[$1]}; p_i++ ))
 	do
 		index[$1]=$p_i
-		Re $(($1+1)) $index
+		Re $(($1+1))
 	done
 }
 
-Re 0
-echo
+#Re 0
+ta=( 1 2 )
+IssueCommand 2 ta[@]
 
+END
 
-: << END
+#: << END
 
 if [ $change -eq 1 ]; then
 
@@ -134,8 +158,7 @@ elif [ $change -eq 6 ] ; then
 	done
 fi
 
-END
-
+#END
 
 
 
