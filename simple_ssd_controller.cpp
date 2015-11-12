91629b9fbd430fb84ff12e95cc6cc77ea88bac03
kalay20 <kalay20@gmail.com>
2015-11-12 04:26:27 -0800

    Old Page Allocation

--- simple_ssd_controller.cpp
+++ simple_ssd_controller.cpp
@@ -15,6 +15,8 @@
 #include "simple_die_scheduler.h"
 #include "simple_die_ftl.h"
 
+#define jiawei_die
+
 using namespace std;
 using namespace sc_core;
 
@@ -240,7 +242,7 @@ void simple_ssd_controller::debug_check_all_req_completed() const {
 	ppn_t total_number_of_gc_page_write_b = 0;
 	// end LaiYang
 	// Ana
-	cout << "**************************** Foreground GC per Die ****************************"<<endl;
+	cout << "**************************** Request per Die ****************************"<<endl;
 	// end Ana
 	for (int die_idx = 0; die_idx < die_schedulers.size(); die_idx++) {
 
@@ -279,16 +281,18 @@ void simple_ssd_controller::debug_check_all_req_completed() const {
                 total_number_of_gc_page_read_b += die_schedulers[die_idx]->number_of_gc_page_read_b;
                 total_number_of_gc_page_write_f += die_schedulers[die_idx]->number_of_gc_page_write_f;
                 total_number_of_gc_page_write_b += die_schedulers[die_idx]->number_of_gc_page_write_b;
-                total_number_of_gc_page_erase_f += die_schedulers[die_idx]->number_of_gc_page_erase_f;
-		// Ana
-		cout << "Die: " << die_idx << "# of foreground_gc: " << die_schedulers[die_idx]->number_of_gc_page_erase_f << endl;                
-		// end Ana
+                total_number_of_gc_page_erase_f += die_schedulers[die_idx]->number_of_gc_page_erase_f;	
 		total_number_of_gc_page_erase_b += die_schedulers[die_idx]->number_of_gc_page_erase_b;
-                // end LaiYang
+		// end LaiYang
+		// Ana
+		cout << "Die: " << die_idx << "\t# of f_gc: " << die_schedulers[die_idx]->number_of_gc_page_erase_f;                
+                cout << "\t# of b_gc: " << die_schedulers[die_idx]->number_of_gc_page_erase_b << "\t\tPage_r: ";
+		cout << die_schedulers[die_idx]->number_of_io_page_read << "\tPage_w: " << die_schedulers[die_idx]->number_of_io_page_write << endl;
+		// end Ana
 
 	}
 	// Ana
-	cout << "************************** end of foreground gc per die ************************" << endl;
+	cout << "************************** end of request per die ************************" << endl;
 	// end Ana
 
 	// LaiYang
@@ -644,7 +648,7 @@ void simple_ssd_controller::on_recv_completed(int bus_id) {
 					die_schedulers[die_index]->schedule_io_page_read(m_recv_buf, die_op_lpn);
 					break;
 				case SSD_REQ_TYP_WR:
-#if 0
+#ifdef jiawei_die
 					die_index = m_die_idx;
 					m_die_idx += ch_array.size();
 					if (m_die_idx > m_number_of_die -1){
@@ -654,7 +658,7 @@ void simple_ssd_controller::on_recv_completed(int bus_id) {
 					die_index = m_die_idx;
 					die_schedulers[die_index]->schedule_io_page_write(m_recv_buf, die_op_lpn);
 					break;
-#elif 1
+#else 					//LaiYang_die
 					die_index = m_die_idx;
 					m_die_idx++;
 					if (m_die_idx > m_number_of_die -1){
@@ -664,6 +668,7 @@ void simple_ssd_controller::on_recv_completed(int bus_id) {
 					//die_index = m_die_idx;
 					die_schedulers[die_index]->schedule_io_page_write(m_recv_buf, die_op_lpn);
 					break;
+					//LaiYang_die end
 #endif
 				default:
 					assert(0);
