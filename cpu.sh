c98be796e9eeb7eb0133905127af565275284dfa
kalay20 <kalay20@gmail.com>
2015-11-14 20:11:15 -0800

    m

--- cpu.sh
+++ cpu.sh
@@ -3,12 +3,12 @@
 
 
 cpu_or_job=${2:-1}
-user=92121
+user=09021
 
 
 if [ $cpu_or_job -eq 1 ]
 then
-	top -b -n1 | grep "90211" | awk '{print $9}' > cpu.dat
+	top -b -n1 | grep "09021" | awk '{print $9}' > cpu.dat
 	#top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'
 	./cpu.out "$1" "$cpu_or_job"
 	if [ $? -eq 0 ]
@@ -19,7 +19,7 @@ then
 	fi
 elif [ $cpu_or_job -eq 2 ]
 then
-	top -b -n1 | grep -E "90211.*run_" | awk '{print $9}' > cpu.dat
+	top -b -n1 | grep -E "09021.*run_" | awk '{print $9}' > cpu.dat
 	#top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'
 	./cpu.out "$1" "$cpu_or_job"
 	if [ $? -eq 0 ]
